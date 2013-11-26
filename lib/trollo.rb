3c86d13345f0e81659f3f56cd9243eb2bc2e61cc
Douglas Anderson <i.am.douglas.anderson@gmail.com>
2013-11-26 13:08:17 +0200

    Misc

--- lib/trollo.rb
+++ lib/trollo.rb
@@ -10,22 +10,8 @@ module Trollo
     
 end
 
-require "trollo/exception"
-require "trollo/invoiceable"
-require "trollo/transaction"
-require "trollo/credit_transaction"
-require "trollo/debit_transaction"
-require "trollo/late_payment"
-require "trollo/line_item"
-require "trollo/payment_reference"
-require "trollo/seller"
-require "trollo/buyer"
-require "trollo/invoice_decorator"
-require "trollo/credit_note_invoice"
-require "trollo/credit_note_credit_transaction"
-
-require "trollo/invoice"
-require "trollo/credit_note"
-require "trollo/overdue_invoice"
-require "trollo/invoice_adjustment"
-require "trollo/line_item_type"
+require "trollo/card"
+require "trollo/list"
+require "trollo/task"
+require "trollo/tasklist"
+require "trollo/troller"
